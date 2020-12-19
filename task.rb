475b4173142af5ae8148609d314c86820b23c504
新田陸人 <>
2020-12-20 05:43:02 +0900

    q16の修正

--- task.rb
+++ task.rb
@@ -142,7 +142,9 @@ def q16
   ]
 
   # 以下に回答を記載
-  users.each { |user| puts "私の名前は#{user[:name]}です。年齢は#{user[:age]}歳です。" }
+  users.each do |user|
+    puts "私の名前は#{user[:name]}です。年齢は#{user[:age]}歳です。"
+  end
 end
 
 class UserQ17
