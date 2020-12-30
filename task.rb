1deea5731efe1dcc0c48063d40511b32ae78e20a
新田陸人 <>
2020-12-31 06:19:14 +0900

    q20のcase文を修正

--- task.rb
+++ task.rb
@@ -232,16 +232,17 @@ class Zoo
   end
 
   def info_entry_fee(user)
-    case user.age
+    admission = case user.age
     when 0..5
-      puts "#{user.name}さんの入場料金は #{@entry_fee[:infant]} 円です。"
+      @entry_fee[:infant]
     when 6..12
-      puts "#{user.name}さんの入場料金は #{@entry_fee[:children]} 円です。"
+      @entry_fee[:children]
     when 13..64
-      puts "#{user.name}さんの入場料金は #{@entry_fee[:adult]} 円です。"
+      @entry_fee[:adult]
     when 65..120
-      puts "#{user.name}さんの入場料金は #{@entry_fee[:senior]} 円です。"
+      @entry_fee[:senior]
     end
+    puts "#{user.name}さんの入場料金は#{admission}円です"
   end
 end
 
